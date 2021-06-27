123b0e6361ff6d4ffff52ee2f9b70d8cb20fb256
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-06-27 18:03:45 +0300

    Register Screen | Basic Functionality

--- app/src/main/java/com/stathis/mydoctor/features/register/RegisterActivity.kt
+++ app/src/main/java/com/stathis/mydoctor/features/register/RegisterActivity.kt
@@ -1,12 +1,16 @@
 package com.stathis.mydoctor.features.register
 
+import android.content.Intent
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.mydoctor.R
 import com.stathis.mydoctor.abstraction.AbstractActivity
+import com.stathis.mydoctor.features.main.MainActivity
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AbstractActivity(R.layout.activity_register) {
 
-    private lateinit var viewModel : RegisterViewModel
+    private lateinit var viewModel: RegisterViewModel
 
     override fun init() {
         viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
@@ -25,10 +29,27 @@ class RegisterActivity : AbstractActivity(R.layout.activity_register) {
     }
 
     override fun running() {
-        //
+        val email = "admin@gmail.com"
+        val pass = "123456"
+        val pass_conf = "123456"
+
+        register_btn.setOnClickListener {
+            when(pass == pass_conf){
+                true -> viewModel.validateData(email,pass)
+                false -> Unit //handle error case with appropriate messages
+            }
+        }
+
+        viewModel.userRegisted.observe(this, Observer {
+            when(it){
+                true -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                }
+                false -> {} // throw some kind of error
+            }
+        })
     }
 
-    override fun stopped() {
-        //
-    }
+    override fun stopped() = viewModel.userRegisted.removeObservers(this)
 }
--- app/src/main/java/com/stathis/mydoctor/features/register/RegisterViewModel.kt
+++ app/src/main/java/com/stathis/mydoctor/features/register/RegisterViewModel.kt
@@ -1,7 +1,29 @@
 package com.stathis.mydoctor.features.register
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterViewModel : ViewModel() {
 
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    val userRegisted = MutableLiveData<Boolean>()
+
+    fun validateData(email: String, pass: String) {
+        when(checkPasswordStrength(pass)){
+            true -> {
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener {
+                    when(it.isSuccessful){
+                        true -> userRegisted.value = true
+                        false -> userRegisted.value = false
+                    }
+                }
+            }
+            false -> {} // throw some kind of error to notify the user
+        }
+    }
+
+    private fun checkPasswordStrength(pass : String) : Boolean {
+        return true
+    }
 }
