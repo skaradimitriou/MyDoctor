0d68fd923d2f9653d808da86c4be768cc5cce9c8
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-07-16 23:44:43 +0300

    Register | Add data to firestore

--- app/src/main/java/com/stathis/mydoctor/features/register/RegisterViewModel.kt
+++ app/src/main/java/com/stathis/mydoctor/features/register/RegisterViewModel.kt
@@ -1,26 +1,55 @@
 package com.stathis.mydoctor.features.register
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.stathis.mydoctor.utils.PasswordValidator
 
 class RegisterViewModel : ViewModel() {
 
     private val auth by lazy { FirebaseAuth.getInstance() }
+    private val firestore by lazy { FirebaseFirestore.getInstance() }
     val userRegisted = MutableLiveData<Boolean>()
 
     fun validateData(email: String, pass: String) {
-        when(PasswordValidator.validatePassword(pass)){
+        when (PasswordValidator.validatePassword(pass)) {
             true -> {
-                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener {
-                    when(it.isSuccessful){
-                        true -> userRegisted.value = true
+                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    when (it.isSuccessful) {
+                        true -> {
+                            saveUserData(email)
+                            userRegisted.value = true
+                        }
                         false -> userRegisted.value = false
                     }
                 }
             }
-            false -> {} // throw some kind of error to notify the user
+            false -> {
+            } // throw some kind of error to notify the user
+        }
+    }
+
+    fun saveUserData(email: String) {
+        val documentReference = firestore.collection("users").document(auth.currentUser!!.uid)
+
+        val data: HashMap<String, Any> = hashMapOf(
+            "username" to email.takeWhile { it != '@' },
+            "location" to "",
+            "userImg" to "",
+            "email" to email,
+            "phoneNo" to ""
+        )
+
+        documentReference.set(data).addOnSuccessListener {
+            Log.d("", "User profile is created for Stathis")
+            userRegisted.value = true
+        }
+
+        documentReference.set(data).addOnFailureListener {
+            Log.d("", "User profile is created for Stathis")
+            userRegisted.value = false
         }
     }
 }
