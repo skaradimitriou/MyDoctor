1c2676b0f3111135ace4b9d4c7eef1e18deb47cb
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-07-23 00:33:39 +0300

    Appointments Fragment | Retrieving user appointments and bind data to screen

--- app/src/main/java/com/stathis/mydoctor/features/main/appointments/AppointmentsViewModel.kt
+++ app/src/main/java/com/stathis/mydoctor/features/main/appointments/AppointmentsViewModel.kt
@@ -1,21 +1,17 @@
 package com.stathis.mydoctor.features.main.appointments
 
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.Source
+import com.google.gson.Gson
 import com.stathis.mydoctor.abstraction.ItemClickListener
-import com.stathis.mydoctor.abstraction.LocalModel
 import com.stathis.mydoctor.callbacks.AppointmentClickListener
 import com.stathis.mydoctor.features.main.appointments.adapter.AppointmentsAdapter
 import com.stathis.mydoctor.models.*
-import com.stathis.mydoctor.utils.TAG
 
 class AppointmentsViewModel : ViewModel(), ItemClickListener {
 
@@ -26,24 +22,18 @@ class AppointmentsViewModel : ViewModel(), ItemClickListener {
     private lateinit var callback : AppointmentClickListener
 
     init {
-        getAppointments()
+        getUserAppointments()
     }
 
-    private fun getAppointments(){
-        firestore.collection("appointments").document(auth.currentUser!!.uid).get()
-            .addOnCompleteListener { task ->
-                when (task.isSuccessful) {
-                    true -> {
-                        val result = task.result
-                        val appointment = result?.toObject(TestAppointment::class.java)
-
-                        val test = result?.get("appointment") as HashMap<String,Doctor>
-
-                        Log.d("","")
-                    }
-                    false -> Unit
+    private fun getUserAppointments() {
+        firestore.collection("appointments")
+            .document(auth.currentUser!!.uid).addSnapshotListener { p0, p1 ->
+                p0?.data?.toList()?.forEach {
+                    val json = Gson().toJsonTree(it.second)
+                    val userAppointments = Gson().fromJson(json, Array<Appointment>::class.java).toMutableList()
+                    list.value = userAppointments
                 }
-        }
+            }
     }
 
     fun bindCallbacks(callback : AppointmentClickListener) {
