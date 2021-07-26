8256370293e2c7efba274180431c3c41ce083b84
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-07-26 23:14:03 +0300

    Reschedule Screen | Basic util of the screen

--- app/src/main/java/com/stathis/mydoctor/features/reschedule/RescheduleActivity.kt
+++ app/src/main/java/com/stathis/mydoctor/features/reschedule/RescheduleActivity.kt
@@ -1,8 +1,21 @@
 package com.stathis.mydoctor.features.reschedule
 
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import com.google.gson.Gson
 import com.stathis.mydoctor.R
 import com.stathis.mydoctor.abstraction.AbstractActivity
+import com.stathis.mydoctor.features.main.MainActivity
+import com.stathis.mydoctor.models.Doctor
+import kotlinx.android.synthetic.main.activity_appointments.*
 
 class RescheduleActivity : AbstractActivity(R.layout.activity_reschedule) {
 
@@ -19,7 +32,96 @@ class RescheduleActivity : AbstractActivity(R.layout.activity_reschedule) {
 
         Note: This activity should pop upwards when open
          */
+
+        val model = intent.getStringExtra("DOCTOR")
+        val oldAppointment = intent.getStringExtra("OLD_APPOINTMENT")
+
+        model?.let {
+            val doctor = Gson().fromJson(it, Doctor::class.java)
+
+            viewModel.setSelectedDoctor(doctor)
+
+            oldAppointment?.let{ viewModel.setOldAppointment(oldAppointment) }
+            bindDoctorInfo(doctor)
+        }
+
+        appointment_save_btn.setOnClickListener{
+            val reason = visit_reason.text.toString()
+            val date = appointment_date.text.toString()
+            val time = appointment_time.text.toString()
+
+            viewModel.validateUserInput(date,time,reason)
+        }
+
+        observeData()
     }
 
-    override fun stopped() {}
+    override fun stopped() = viewModel.release(this)
+
+    private fun observeData() {
+        viewModel.appointmentUpdated.observe(this, Observer {
+            when (it) {
+                true -> notifyAndExit()
+                false -> Unit
+            }
+        })
+
+        viewModel.appointmentHour.observe(this,Observer{
+            appointment_time.text = it
+        })
+
+        viewModel.appointmentDate.observe(this, Observer {
+            appointment_date.text = it
+        })
+    }
+
+    private fun notifyAndExit() {
+        Toast.makeText(this,"Appointment Saved", Toast.LENGTH_LONG).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 1500)
+    }
+
+    private fun bindDoctorInfo(model: Doctor) {
+        Glide.with(this).load(model.image).into(doctor_img)
+        doctor_name.text = model.fullname
+        doctor_category.text = model.category
+        appointment_location.text = resources.getString(R.string.dummy_doc_hospital)
+
+        selectDates()
+        selectTime()
+    }
+
+    private fun selectTime() {
+        val picker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(10)
+            .setTitleText("Select Appointment time")
+            .build()
+
+        appointment_time.setOnClickListener {
+            picker.show(this.supportFragmentManager, "SELECT TIME")
+            picker.addOnPositiveButtonClickListener {
+                val hour = picker.hour
+                val minute = picker.minute
+
+                viewModel.validateTime(hour, minute)
+            }
+        }
+    }
+
+    private fun selectDates() {
+        val dateRangePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Ημερομηνία Ραντεβού")
+            .build()
+
+        appointment_date.setOnClickListener {
+            dateRangePicker.show(this.supportFragmentManager, "SELECT DATES")
+            dateRangePicker.addOnPositiveButtonClickListener {
+                viewModel.validateDate(it)
+            }
+        }
+    }
 }
--- app/src/main/java/com/stathis/mydoctor/features/reschedule/RescheduleViewModel.kt
+++ app/src/main/java/com/stathis/mydoctor/features/reschedule/RescheduleViewModel.kt
@@ -1,11 +1,16 @@
 package com.stathis.mydoctor.features.reschedule
 
+import android.text.format.DateFormat
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.common.api.internal.LifecycleCallback
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
 import com.stathis.mydoctor.models.Appointment
+import com.stathis.mydoctor.models.Doctor
+import java.util.*
 
 class RescheduleViewModel : ViewModel() {
 
@@ -13,6 +18,10 @@ class RescheduleViewModel : ViewModel() {
     private val auth by lazy { FirebaseAuth.getInstance() }
     private var userAppointments = mutableListOf<Appointment>()
     val appointmentUpdated = MutableLiveData<Boolean>()
+    val appointmentHour = MutableLiveData<String>()
+    val appointmentDate = MutableLiveData<String>()
+    private lateinit var doctor: Doctor
+    private lateinit var oldAppointment: Appointment
 
     init {
         getUserAppointments()
@@ -28,24 +37,62 @@ class RescheduleViewModel : ViewModel() {
             }
     }
 
-    fun updateAppointment(appointment: Appointment){
-        when(userAppointments.contains(appointment)){
-            true ->  {
-                //FIXME: update the appointment with the given hours
+    private fun saveAppointment(date : String, time : String, reason : String){
+        val appointment = Appointment(doctor, date, time, reason)
+
+        val newAppointmentList = mutableListOf<Appointment>()
+        userAppointments.forEach {
+            when(it != oldAppointment){
+                true -> {
+                    newAppointmentList.add(it)
+                }
             }
-            false -> Unit
         }
-    }
 
-    private fun saveToDatabase(){
+        newAppointmentList.add(0,appointment)
+
         val docRef = firestore.collection("appointments").document(auth.currentUser!!.uid)
-
         val data = hashMapOf<String, Any>(
-            "appointments" to userAppointments
+            "appointments" to newAppointmentList
         )
 
         docRef.set(data).addOnSuccessListener {
             appointmentUpdated.value = true
         }
+    }
+
+    fun setSelectedDoctor(doctor: Doctor) {
+        this.doctor = doctor
+    }
+
+    fun validateUserInput(date: String, time: String, reason: String) {
+        when (date.isNullOrEmpty() and time.isNullOrEmpty() and reason.isNullOrEmpty()) {
+            true -> {
+            } //throw message
+            false -> saveAppointment(date, time, reason)
+        }
+    }
+
+    fun validateTime(hour: Int, minute: Int) {
+        when (minute) {
+            0 -> appointmentHour.value = "$hour:00"
+            else -> appointmentHour.value = "$hour:$minute"
+        }
+    }
+
+    fun validateDate(date: Long) {
+        val fullDate: String = DateFormat.format("dd/MM/yyyy", Date(date)).toString()
+        appointmentDate.value = fullDate
+    }
+
+    fun setOldAppointment(oldAppointment: String) {
+        val model = Gson().fromJson(oldAppointment, Appointment::class.java)
+        this.oldAppointment = model
+    }
+
+    fun release(owner: LifecycleOwner) {
+        appointmentUpdated.removeObservers(owner)
+        appointmentDate.removeObservers(owner)
+        appointmentHour.removeObservers(owner)
     }
 }
