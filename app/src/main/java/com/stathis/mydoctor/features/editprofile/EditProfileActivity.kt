76b714345f65770d92ae357fbaeafc1cbba44e43
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-08-02 23:25:49 +0300

    Onboarding & EditProfile Screens | Init

---
+++ app/src/main/java/com/stathis/mydoctor/features/editprofile/EditProfileActivity.kt
@@ -0,0 +1,19 @@
+package com.stathis.mydoctor.features.editprofile
+
+import com.stathis.mydoctor.R
+import com.stathis.mydoctor.abstraction.AbstractActivity
+
+class EditProfileActivity : AbstractActivity(R.layout.activity_edit_profile) {
+
+    override fun init() {
+        //
+    }
+
+    override fun running() {
+        //
+    }
+
+    override fun stopped() {
+        //
+    }
+}
---
+++ app/src/main/java/com/stathis/mydoctor/features/onboarding/OnboardingActivity.kt
@@ -0,0 +1,19 @@
+package com.stathis.mydoctor.features.onboarding
+
+import com.stathis.mydoctor.R
+import com.stathis.mydoctor.abstraction.AbstractActivity
+
+class OnboardingActivity : AbstractActivity(R.layout.activity_onboarding) {
+
+    override fun init() {
+        //
+    }
+
+    override fun running() {
+        //
+    }
+
+    override fun stopped() {
+        //
+    }
+}
