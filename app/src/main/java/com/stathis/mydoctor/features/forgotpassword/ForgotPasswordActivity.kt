8a3eaf78c64054a7f924ab29a3ddfa4493457056
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-08-31 23:37:26 +0300

    Forgot Password | Added basic logic

--- app/src/main/java/com/stathis/mydoctor/features/forgotpassword/ForgotPasswordActivity.kt
+++ app/src/main/java/com/stathis/mydoctor/features/forgotpassword/ForgotPasswordActivity.kt
@@ -1,10 +1,11 @@
 package com.stathis.mydoctor.features.forgotpassword
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.stathis.mydoctor.R
 import com.stathis.mydoctor.abstraction.AbstractActivity
+import de.mateware.snacky.Snacky
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AbstractActivity(R.layout.activity_forgot_password) {
 
@@ -15,11 +16,27 @@ class ForgotPasswordActivity : AbstractActivity(R.layout.activity_forgot_passwor
     }
 
     override fun running() {
-        //
+        send_activation_email.setOnClickListener {
+            viewModel.verifyEmail(forgot_email_field.text.toString())
+        }
+
+        viewModel.userVerified.observe(this, Observer {
+            when (it) {
+                true -> showPopup("An e-mail has been sent to the above address.")
+                false -> { } //handle this
+            }
+        })
     }
 
     override fun stopped() {
-
+        viewModel.userVerified.removeObservers(this)
     }
+    private fun showPopup(message : String) {
+        Snacky.builder().setView(findViewById(R.id.forgot_pass_screen)).success().setText(message)
+            .show()
 
+        /*
+        FIXME: consider adding onBackPressed after some time
+         */
+    }
 }
--- app/src/main/java/com/stathis/mydoctor/features/forgotpassword/ForgotPasswordViewModel.kt
+++ app/src/main/java/com/stathis/mydoctor/features/forgotpassword/ForgotPasswordViewModel.kt
@@ -1,7 +1,37 @@
 package com.stathis.mydoctor.features.forgotpassword
 
 import android.app.Application
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
 import com.stathis.mydoctor.abstraction.AbstractAndroidViewModel
 
 class ForgotPasswordViewModel(app : Application) : AbstractAndroidViewModel(app) {
+
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    val userVerified = MutableLiveData<Boolean>()
+
+    fun verifyEmail(email : String) {
+        auth.fetchSignInMethodsForEmail(email)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val isNewUser: Boolean? = it.result?.signInMethods?.isEmpty()
+                    if (isNewUser!!) {
+                        Log.e("TAG", "Is New User!")
+                        userVerified.value = false
+                    } else {
+                        sendEmail(email)
+                    }
+                }
+            }
+    }
+
+    private fun sendEmail(email : String) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    userVerified.value = true
+                }
+            }
+    }
 }
